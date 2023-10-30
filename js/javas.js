0fd83e13ae8bbead565381e8987f43b6ee5b8739
lucianocaro1 <143449214+lucianocaro1@users.noreply.github.com>
2023-10-30 15:22:06 -0300

    Add files via upload

---
+++ js/javas.js
@@ -0,0 +1,6 @@
+function cartel() {alert("Mi nombre es Luciano Javier Caro, tengo 26 añosy soy estudiante de Ingenieria en Sistemas en la UNS.")}
+
+function modoNoc() {
+    document.body.style.backgroundColor = "black";
+    document.header.style.backgroundColor = "black";
+}
